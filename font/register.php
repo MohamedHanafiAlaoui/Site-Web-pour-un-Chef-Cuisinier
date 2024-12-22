1277838fbe8078483e5cf74c21672e299a393fdf
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2024-12-22 16:47:22 +0100

    create page register , crete new user

---
+++ font/register.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<?php
+include 'conect_db.php';
+
+
+if (
+    isset($_POST['LastName'], $_POST['nom'], $_POST['Email'], $_POST['Password'], $_POST['cin'], $_POST['inputDate'])
+) {
+
+
+    $LastName = trim($_POST['LastName']);
+    $FristName = trim($_POST['nom']);
+    $Email = trim($_POST['Email']);
+    $Password = trim($_POST['Password']);
+    $cin = trim($_POST['cin']);
+    $FristDate = trim($_POST['inputDate']);
+
+    $query = "insert into Users(FristName,LastName,Password,cin,Email,FirstDate ) values ('$FristName','$LastName','$Password' ,'$cin','$Email','$FristDate')";
+
+    mysqli_query($conn,$query);
+    $query2 = "insert into ROLES(cin,TYPE) values ('$cin','user' )";
+
+    mysqli_query($conn,$query2);
+
+    header("Location:login.php");
+
+
+}
